ecfb5ef0d5008b1c80eb1f37da461276f49ddfe7
Joanne Hugé <joanne.huge@nexedi.com>
2023-05-13 22:14:18 +0200

    Front: fix footer links

--- tagforest-frontend-vue/src/components/FooterComponent.vue
+++ tagforest-frontend-vue/src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <footer><div class="container">
     <span>Version <!--CURRENT_VERSION--> (<!--CURRENT_VERSION_DATE-->)</span> -
-    <router-link to="https://api.tagforest.fr">REST API</router-link> -
-    <router-link to="https://github.com/jhg8/tagforest">Github</router-link>
+    <a href="https://api.tagforest.fr">REST API</a> -
+    <a href="https://github.com/jhg8/tagforest">Github</a>
   </div></footer>
 </template>
